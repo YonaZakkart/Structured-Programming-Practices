038f3f3d177b8f8b3b71c02915d20cf5dbb556ae
YonaZakkart <musicyonathan.com>
2025-08-20 13:43:57 -0600

    Creando Repositorio

---
+++ Practice01/index.js
@@ -0,0 +1,13 @@
+//a 
+
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name? ", (name) => {
+    console.log(`Hello, ${name} Welcome!`);
+    rl.close();
+})
